f37046d0a9e6990642a79bd94861667f54ffa2c7
guilhermesilvasanttos <127353872+guilhermesilvasanttos@users.noreply.github.com>
2024-09-25 22:17:56 -0300

    Add files via upload

---
+++ Musicas/Musicas/src/main/java/br/edu/univille/poo/tarefas/entity/Compartilhamento.java
@@ -0,0 +1,21 @@
+package br.edu.univille.poo.tarefas.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Compartilhamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    private Conta compartilhadoPor;
+    @ManyToOne
+    private Conta recebidoPor;
+    @ManyToOne
+    private Playlist playlist;
+}
